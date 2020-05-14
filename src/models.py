a03c3fb80694349ac65a142b429494931a5cdcf5
xCloudx8 <danielestrife@gmail.com>
2020-05-14 14:48:06 +0200

    Working version with RMSE

--- src/models.py
+++ src/models.py
@@ -1,49 +1,4 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 from sklearn import linear_model
 from sklearn.metrics import max_error
 import math 
 
-def keras_sequential(train_dataset):
- 
-  model = keras.Sequential([
-    layers.Dense(100, activation='linear', 
-    input_shape=[len(train_dataset.keys())]),
-    layers.Dense(1)
-  ])
-
-  optimizer = tf.keras.optimizers.Adamax(learning_rate=0.35, beta_1=0.9, beta_2=0.999)
-
-  model.compile(loss='mse',
-                optimizer=optimizer,
-                metrics=['mae', 'mse'])
-  return model
-
-def linearRegressor(X, y, de):
-  X = X[de:]
-  y = y[de:]
-
-  linearRegr = linear_model.LinearRegression()
-  linearRegr.fit(X,y)
-  score = linearRegr.score(X, y)
-
-  yPred = linearRegr.predict(X)
-  error = max_error(y, yPred)
-
-  Xtest = []
-  gp = 40
-
-  for i in range(de, de+gp):
-    Xtest.append([i])
-  
-  yPred_linear = linearRegr.predict(Xtest)
-
-  yPredMax = []
-  yPredMin = []
-
-  for i in range(0, len(yPred_linear)):
-    yPredMax.append(yPred_linear[i] + error)
-    yPredMin.append(yPred_linear[i] - error)
-  
-  return yPredMax, yPredMin, yPred_linear, Xtest
--- src/predictor.py
+++ src/predictor.py
@@ -1,34 +1,135 @@
-import numpy as np 
-import pandas as pd
+from datetime import datetime
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-from . import models
-from src import dataAnalysis
-from sklearn import linear_model
+import numpy as np
+import os
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
 
+'''
+    Auxiliary functions
+'''
+def dataFrame():
+    df = pd.read_csv('data/dataCsv.csv', sep=',', header=0)
+    return df
+
+def test_stationarity(timeseries):
+    
+    #Determing rolling statistics
+    rolmean = pd.Series(timeseries).rolling(window=3).mean()
+    rolstd = pd.Series(timeseries).rolling(window=3).std()
+#Plot rolling statistics:
+    plt.plot(timeseries, color='blue',label='Original')
+    plt.plot(rolmean, color='red', label='Rolling Mean')
+    plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show()
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
+
+def seasonal_dec(ts_log):
+    decomposition = seasonal_decompose(ts_log, period=12)
+    trend = decomposition.trend
+    seasonal = decomposition.seasonal
+    residual = decomposition.resid
+    '''
+    plt.subplot(411)
+    plt.plot(ts_log, label= 'Original')
+    plt.legend(loc='best')
+    plt.subplot(412)
+    plt.plot(trend, label = 'Trend')
+    plt.legend(loc='best')
+    plt.subplot(413)
+    plt.plot(seasonal, label='Seasonality')
+    plt.legend(loc='best')
+    plt.subplot(414)
+    plt.plot(residual, label='Residual')
+    plt.legend(loc='best')
+    plt.tight_layout()
+    '''
+
+    ts_log_decompose =  residual
+    ts_log_decompose.dropna(inplace=True)
+    test_stationarity(ts_log_decompose)
+
+def forecasting(ts_log_diff):
+    lag_acf = acf(ts_log_diff, nlags=12)
+    lag_pacf = pacf(ts_log_diff, nlags = 12, method = 'ols')
+
+    #Plot Acf
+    plt.subplot(121)
+    plt.plot(lag_acf)
+    plt.axhline(y=0, linestyle='--', color='gray')
+    plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)), linestyle='--', color='gray')
+    plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)), linestyle='--', color='gray')
+    plt.title('Autocorrelation Function')
+
+    #Plot PACF
+    plt.subplot(122)
+    plt.plot(lag_pacf)
+    plt.axhline(y=0, linestyle='--', color='gray')
+    plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)), linestyle='--', color='gray')
+    plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)), linestyle='--', color='gray')
+    plt.title('Partial Autocorrelation Function')
+    plt.tight_layout()
+    plt.show()
+
+def ARmodel(ts_log, ts_log_diff):
+    model = ARIMA(ts_log,order=(2,1,0))
+    results_ar = model.fit(disp=-1)
+    plt.plot(ts_log_diff)
+    plt.plot(results_ar.fittedvalues, color='red')
+    plt.title('RSS: %.4f' % sum((results_ar.fittedvalues - ts_log_diff)**2))
+    plt.show()
+    return results_ar.fittedvalues
+    
 def predict():
-    df = dataAnalysis.dataFrame()
-    total_cases = df['totale_casi']
-    total_analysis = df['tamponi']
+    df = dataFrame()
+    con = df['data']
+    df['data']=pd.to_datetime(df['data'])
+    df.set_index('data', inplace= True)
+    ts = df['totale_positivi']
+    #test_stationarity(ts)
 
-    y = []
-    ta_increase = []
-
-    for i in range(1,len(total_analysis)):
-        actEPI = (total_cases[i] - total_cases[i-1])/ (total_analysis[i] - total_analysis[i-1])*100
-        ta_increase.append(total_analysis[i]-total_analysis[i-1])
-        y.append(actEPI)
+    ts_log = np.log(ts)
+    moving_avg = pd.Series(ts_log).rolling(window=3).mean()
     
-    X = []
-    for i in range(1, len(y)+1):
-        X.append([i])
-
-    de = 14 + 7
+    ts_log_moving_avg_diff = ts_log - moving_avg
+    ts_log_moving_avg_diff.dropna(inplace=True)
     
-    yPredMax, yPredMin, yPred_linear, Xtest = models.linearRegressor(X, y, de)
+    expwighted_avg = pd.Series(ts_log).ewm( halflife = 3).mean()
+    
+    ts_log_ewma_diff = ts_log - expwighted_avg
+    
+    ts_log_diff = ts_log - ts_log.shift()
+    
+    ts_log_diff.dropna(inplace = True)
+    
+    seasonal_dec(ts_log)
+    forecasting(ts_log_diff)
+    arimaFitted = ARmodel(ts_log, ts_log_diff)
 
-    X = X[de:]
-    y = y[de:]
-    dataAnalysis.plottingLinear(df, yPred_linear, yPredMax, yPredMin, de, Xtest, X, y)
+    prediction_ARIMA_diff = pd.Series(arimaFitted, copy= True)
+    predictions_ARIMA_diff_cumsum = prediction_ARIMA_diff.cumsum()
+    prediction_ARIMA_LOG = pd.Series(ts_log[0], index = ts_log.index)
+    prediction_ARIMA_LOG = prediction_ARIMA_LOG.add(predictions_ARIMA_diff_cumsum, fill_value=0)
+
+    prediction_ARIMA = np.exp(prediction_ARIMA_LOG)
+    plt.close()
+    plt.plot(ts)
+    plt.plot(prediction_ARIMA)
+    plt.title('RMSE: %4.f'% np.sqrt(sum((prediction_ARIMA-ts)**2)/len(ts)))
+    plt.show()
+    pass
 
 if __name__ == '__main__':
-    pass
+    predict()
